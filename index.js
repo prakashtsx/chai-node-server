4ce04d04bfbae857454bbedfef0882c58999830e
prakashtsx <urprakashgupta@gmail.com>
2026-06-07 13:40:31 +0530

    feat: update health check status message to include GitHub Actions

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT ?? 8000;
 app.get('/', (req, res) => {
     return res.json({
         message: "Server is healthy V2",
-        status: "success"
+        status: "success github actions"
     })
 })
 
